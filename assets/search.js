a08d8d2bce38e14926bbeb7f2ad2e42c40f961a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-20 16:21:47 +0000

    Deploy brunoprocopio/blog to github.com/brunoprocopio/blog.git:gh-pages

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"React 101 (by a back-end developer)",'url':"/2020/11/19/react-101.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"React 101 (by a back-end developer)",'url':"/2020/11/20/react-101.html"}]};
