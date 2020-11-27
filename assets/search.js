00da2e5fa3fd033cf700435fcb98cc8d7aaa1a83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 18:12:33 +0000

    Deploy brunoprocopio/blog to github.com/brunoprocopio/blog.git:gh-pages

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"React 101 (by a back-end developer)",'url':"/2020/11/20/react-101.html"},{'title':"What is JSX and TSX",'url':"/2020/11/26/jsx-and-tsx.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"React 101 (by a back-end developer)",'url':"/2020/11/20/react-101.html"},{'title':"What is JSX and TSX",'url':"/2020/11/27/jsx-and-tsx.html"}]};
